8a69b7017d5a89648946120294bc8422c0cdea45
Damon Maldonado <damon@mpwmarketing.com>
2016-03-02 10:10:44 -0500

    initial plugin

---
+++ mpw-conversion-buttons.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: MPW Convversion Buttons
+Description: Shortcode for conversion buttons
+Version: 0.1.0
+*/
+
+function mpw_conv_button ( $atts ) {
+	extract( shortcode_atts( array(
+		'link' => '#',
+		'text'  => '',
+		'add_class' => '',
+		), $atts));
+$cont = '';
+$cont .= '<div class="conv-btn '.$add_class.'" onclick="location.href=\''.$link.'\'">';
+$cont .= '<a href="'.$link.'">'.$text.'</a>';
+$cont .= '</div>';
+
+return $cont;
+}
+
+add_shortcode( $'conv_button' , 'mpw_conv_button' );
