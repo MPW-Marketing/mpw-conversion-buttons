7240d8062ad43d36c381e84645de7f356d69a881
Damon Maldonado <damon@mpwmarketing.com>
2016-03-03 11:22:34 -0500

    script updates

--- js/mpw-conversion-buttons.js
+++ js/mpw-conversion-buttons.js
@@ -1,4 +1,9 @@
-jQuery('.conv-btn.new-win').click(function(e){
-	e.preventDefault(); window.open(jQuery(this).children("a").prop("href"),"_blank");
+jQuery('.conv-btn').click(function(e){
+	e.preventDefault(); 
+	if(jQuery(this).children("a").prop("target") === "_blank"){
+		window.open(jQuery(this).children("a").prop("href"),"_blank");
+	} else {
+		window.open(jQuery(this).children("a").prop("href"));
+	}	
 }
 );
